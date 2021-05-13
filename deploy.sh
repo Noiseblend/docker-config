210d55bbaf56b10c713cfb997200a71d27ab8d6f
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-05-13 14:36:15 +0300

    Drone

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [[ "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "main" ]]; then
-    curl --basic --user alin:$DEPLOY_PASSWORD --get --data-urlencode "update=$UPDATE_COMMAND" --data-urlencode "restart=$RESTART" --data-urlencode "async=$ASYNC" https://deploy.darkwoods.win/$STACK
-fi
